82c41e3a254fb9c8b5206c5be63fa1827a7c070f
Radu Cristescu <radu.cx@gmail.com>
2022-09-22 04:18:48 +0100

    Window: handle Enter/Escape keypresses

--- src/components/Window/Window.js
+++ src/components/Window/Window.js
@@ -1,11 +1,49 @@
 import { t } from 'i18next';
+import { useEffect } from 'react';
 import { useCentredDiv } from '../../lib/useCentredDiv';
 import { useWindowDimensions } from '../../lib/useWindowDimensions';
 import styles from './Window.module.css';
 
-function Window({ title, onOK, showThrobber, open, children }) {
+function Window({ title, onResult, showThrobber, open, children }) {
   const dimensions = useWindowDimensions();
   const [position, ref] = useCentredDiv();
+
+  useEffect(() => {
+    if (!ref.current || !open) {
+      return;
+    }
+
+    const formElements = ["input", "select", "textarea"];
+    const all = ref.current.getElementsByTagName("*");
+
+    for (let i = 0; i < all.length; i++) {
+      if (formElements.indexOf(all[i].tagName.toLowerCase()) !== -1) {
+        all[i].focus();
+        break;
+      }
+    }
+  }, [ref, open]);
+
+  useEffect(() => {
+    function key(e) {
+      if (!open) {
+        return;
+      }
+
+      if (e.keyCode === 13) {
+        onResult(true);
+      }
+
+      if (e.keyCode === 27) {
+        onResult(false);
+      }
+    }
+
+    document.addEventListener('keydown', key);
+    return () => {
+      document.removeEventListener('keydown', key);
+    };
+  });
 
   if (!open) {
     return <></>;
@@ -28,8 +66,8 @@ function Window({ title, onOK, showThrobber, open, children }) {
           {title}
         </div>
         <div className={styles.windowcontent}>{children}</div>
-        <input type="button" className={styles.windowok} value={t('windowok')} onClick={onOK} />
-        <input type="button" className={styles.windowcancel} value={t('windowcancel')} style={{ visibility: onOK ? "" : "hidden" }} />
+        <input type="button" className={styles.windowok} value={t('windowok')} onClick={() => onResult(true)} />
+        <input type="button" className={styles.windowcancel} value={t('windowcancel')} style={{ visibility: onResult ? "" : "hidden" }} onClick={() => onResult(false)} />
       </div>
     </>
   );
