2662bd8a9f53b328fe8cc32c60d8b0006625018c
Radu Cristescu <radu.cx@gmail.com>
2022-09-22 16:10:51 +0100

    Replace hardcoded element IDs with the useID hook

--- src/components/Options/Options.js
+++ src/components/Options/Options.js
@@ -1,78 +1,85 @@
 import { t } from 'i18next';
+import { useId } from 'react';
 import styles from './Options.module.css';
 
 function Options() {
+  const baseID = useId();
+
+  function id(name) {
+    return `${baseID}-${name}`;
+  }
+
   return (
     <>
       <div className={styles.opts}>
         <table>
           <tbody>
             <tr>
-              <td>* <label htmlFor="optionlocale">{t('language')}</label></td>
+              <td>* <label htmlFor={id("optionlocale")}>{t('language')}</label></td>
               <td>
-                <select id="optionlocale">
+                <select id={id("optionlocale")}>
                   <option></option>
                 </select>
               </td>
             </tr>
             <tr>
-              <td>* <label htmlFor="optiondb">{t('db')}</label></td>
+              <td>* <label htmlFor={id("optiondb")}>{t('db')}</label></td>
               <td>
-                <select id="optiondb">
+                <select id={id("optiondb")}>
                   <option></option>
                 </select>
               </td>
             </tr>
             <tr>
               <td>
-                <label htmlFor="optionsnap">{t('snap')}</label>
+                <label htmlFor={id("optionsnap")}>{t('snap')}</label>
               </td>
               <td>
-                <input type="text" size="4" id="optionsnap" />
+                <input type="text" size="4" id={id("optionsnap")} />
                 <span className={styles.small}>{t('optionsnapnotice')}</span>
               </td>
             </tr>
             <tr>
               <td>
-                <label htmlFor="optionpattern">{t('pattern')}</label>
+                <label htmlFor={id("optionpattern")}>{t('pattern')}</label>
               </td>
               <td>
-                <input type="text" size="6" id="optionpattern" />
+                <input type="text" size="6" id={id("optionpattern")} />
                 <span className={styles.small}>{t('optionpatternnotice')}</span>
               </td>
             </tr>
             <tr>
-              <td>* <label htmlFor="optionstyle">{t('style')}</label></td>
+              <td>* <label htmlFor={id("optionstyle")}>{t('style')}</label></td>
               <td>
-                <select id="optionstyle">
+                <select id={id("optionstyle")}>
                   <option></option>
                 </select>
               </td>
             </tr>
             <tr>
               <td>
-                <label htmlFor="optionhide">{t('hide')}</label>
+                <label htmlFor={id("optionhide")}>{t('hide')}</label>
               </td>
               <td>
-                <input type="checkbox" id="optionhide" />
-              </td>
-            </tr>
-            <tr>
-              <td>* <label htmlFor="optionvector">{t('vector')}</label></td>
-              <td>
-                <input type="checkbox" id="optionvector" />
+                <input type="checkbox" id={id("optionhide")} />
               </td>
             </tr>
             <tr>
-              <td>* <label htmlFor="optionshowsize">{t('showsize')}</label></td>
+              <td>* <label htmlFor={id("optionvector")}>{t('vector')}</label></td>
               <td>
-                <input type="checkbox" id="optionshowsize" />
+                <input type="checkbox" id={id("optionvector")} />
               </td>
             </tr>
             <tr>
-              <td>* <label htmlFor="optionshowtype">{t('showtype')}</label></td>
+              <td>* <label htmlFor={id("optionshowsize")}>{t('showsize')}</label></td>
               <td>
-                <input type="checkbox" id="optionshowtype" />
+                <input type="checkbox" id={id("optionshowsize")} />
+              </td>
+            </tr>
+            <tr>
+              <td>* <label htmlFor={id("optionshowtype")}>{t('showtype')}</label></td>
+              <td>
+                <input type="checkbox" id={id("optionshowtype")} />
               </td>
             </tr>
           </tbody>
